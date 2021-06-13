d4cc64cc61860f1d97f3221aed56be01c39dad50
Alejandro Garcia Vera <alejandrogarciav96@gmail.com>
2021-06-13 21:16:25 +0200

    added test class and tested queue and stack data structures

--- Program/src/main.cpp
+++ Program/src/main.cpp
@@ -1,14 +1,50 @@
 #include<iostream>
 #include"mystack.cpp"
+#include"myQueue.cpp"
+
+struct test {
+	int x, y;
+	test() :x{NULL}, y{NULL} {};
+	test(int x, int y) :x(x), y(y) {};
+
+	friend std::ostream& operator<<(std::ostream& os, const test& tst);
+	test(int) :x{ NULL }, y{ NULL } {};
+};
+
+std::ostream& operator<<(std::ostream& os, const test& tst)
+{
+	os << "{" << tst.x << ", " << tst.y << "}";
+	return os;
+}
 
 int main() {
-	Stack<int> x;
-	for (int i = 0; i < 5; i++)
+	
+	Stack<test> stackTest;
+	for (int i = 0; i < 10; i++)
 	{
-		x.push(i);
+		stackTest.push({i,i});
 	}
-	x.display();
-	std::cout << x.peek() << std::endl;
-	x.pop();
-	x.display();
+	std::cout << stackTest.peek() << std::endl;
+	stackTest.display();
+	for (int i = 0; i < 10; i++)
+	{
+		stackTest.pop();
+	}
+	std::cout << stackTest.peek() << std::endl;
+	stackTest.display();
+	
+	Queue<test> queueTest;
+	for (int i = 0; i < 10; i++)
+	{
+		queueTest.push({ i,i });
+	}
+	std::cout << queueTest.peek() << std::endl;
+	queueTest.display();
+	for (int i = 0; i < 10; i++)
+	{
+		queueTest.pop();
+	}
+	std::cout << queueTest.peek() << std::endl;
+	queueTest.display();
+
 }
